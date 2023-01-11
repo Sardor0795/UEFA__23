5f292f4f9a31d70d41d07f0f7f6c924077a437ff
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2023-01-12 00:35:26 +0500

    Finish Project

--- script/script.js
+++ script/script.js
@@ -41,8 +41,17 @@ let ballPath = {
   9: { b: 105, l: 760 },
 };
 
+let outPath = {
+  1: { b: 320, l: 580 },
+  2: { b: 320, l: 180 },
+  3: { b: 250, l: 180 },
+  4: { b: 329, l: 797 },
+  5: { b: 155, l: 788 },
+};
+
 function winFunc() {
   win.innerHTML = "You Win";
+  win.style.display = "block";
   playGameAgainBtn.style.display = "block";
   constBtn.style.display = "none";
   gameOver.play();
@@ -61,6 +70,7 @@ function playAgain() {
   gameOver.load();
   goalSave.load();
   goalSound.load();
+  stadiumSound.play();
   constBtn.innerHTML = "Start Game";
   constBtn.style.display = "block";
   playGameAgainBtn.style.display = "none";
@@ -137,12 +147,80 @@ pitch.addEventListener("click", (e) => {
     e.offsetY >= 520 &&
     e.offsetY <= 570
   ) {
+    if (e.offsetY > 562) {
+      stadiumSound.load();
+      ballSound.play();
+      let outNum = parseInt(Math.random() * 5);
+
+      ball.style.cssText = `
+      bottom: ${outPath[outNum + 1].b}px;
+      left: ${outPath[outNum + 1].l}px;
+      width: 30px;
+      height: 30px;
+    `;
+
+      goalKeeper.style.cssText = `
+      bottom: ${goalPath[randNumGoal + 1].b}px;
+      left: ${goalPath[randNumGoal + 1].l}px;
+      transform: rotate(${goalPath[randNumGoal + 1].r}deg);
+    `;
+      setTimeout(() => {
+        goalPostHit.play();
+      }, 300);
+
+      pc += 1;
+      gameCount += 1;
+
+      pcScore.innerHTML = pc;
+      pcScore.style.color = "#f12d0b";
+      loose.style.display = "block";
+      loose.innerHTML = "OUT !!!";
+
+      if (gameCount >= 5 && score > pc) {
+        loose.style.display = "none";
+        winFunc();
+      } else if (gameCount >= 5 && score < pc) {
+        looseFunc();
+      }
+
+      setTimeout(() => {
+        modal.style.display = "flex";
+        modal.style.oppacity = "1";
+        modal.style.pointerEvents = "all";
+        modal.style.zIndex = "500";
+        constBtn.innerHTML = "Continue";
+        foot.style.height = "0";
+        foot.style.width = "0";
+      }, 500);
+
+      return false;
+    } else if (e.offsetX <= 470 && e.offsetY <= 540) {
+      randNumBall = 8;
+    } else if (e.offsetX <= 485 && e.offsetY <= 540) {
+      randNumBall = 5;
+    } else if (e.offsetX < 500 && e.offsetY <= 540) {
+      randNumBall = 2;
+    } else if (e.offsetX <= 470 && e.offsetY <= 555) {
+      randNumBall = 7;
+    } else if (e.offsetX <= 485 && e.offsetY <= 555) {
+      randNumBall = 4;
+    } else if (e.offsetX < 500 && e.offsetY <= 555) {
+      randNumBall = 1;
+    } else if (e.offsetX <= 470 && e.offsetY < 570) {
+      randNumBall = 6;
+    } else if (e.offsetX <= 485 && e.offsetY < 570) {
+      randNumBall = 3;
+    } else if (e.offsetX < 500 && e.offsetY < 570) {
+      randNumBall = 0;
+    }
+
     ballSound.play();
 
     ball.style.cssText = `
       bottom: ${ballPath[randNumBall + 1].b}px;
       left: ${ballPath[randNumBall + 1].l}px;
       width: 30px;
+      height: 30px;
     `;
 
     goalKeeper.style.cssText = `
@@ -185,7 +263,7 @@ pitch.addEventListener("click", (e) => {
         }
         return false;
       }
-      // stadiumSound.load();
+      stadiumSound.load();
       goalSound.play();
       score += 1;
       gameCount += 1;
@@ -224,7 +302,7 @@ pitch.addEventListener("click", (e) => {
 });
 
 const continueGame = () => {
-  // stadiumSound.play();
+  stadiumSound.play();
   ball.style.cssText = `
   bottom: 10px;
   left: calc(50% - 25px);
@@ -239,15 +317,11 @@ const continueGame = () => {
   modal.style.pointerEvents = "none";
   modal.style.zIndex = "-1";
 
+  loose.innerHTML = "NOOOO !!!";
   win.style.display = "none";
   loose.style.display = "none";
   goalSound.load();
 };
-
-// x: 455 - 500
-// y 520 - 570
-
-// Enter game
 
 let enterGameDisplay = document.querySelector(".enterGame");
 
@@ -285,6 +359,7 @@ const quitGame = () => {
   gameOver.load();
   goalSave.load();
   goalSound.load();
+  stadiumSound.load();
   loose.innerHTML = "NOOOO !!!";
   win.innerHTML = "GOOOOOL";
   constBtn.innerHTML = "Start Game";
